ab69f78615e487c002365294703fc8209a8e83ee
mdevoe12 <matthew.devoe@github.com>
2021-07-18 15:03:30 -0600

    Added Welcome.vue

---
+++ src/components/Welcome.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Welcome {{name}}</h1>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String
+  }
+}
+</script>
+
+<style scoped>
+  .h1 {
+    font: bold
+  }
+</style>
